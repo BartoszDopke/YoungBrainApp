6f1dc670097577fb9ec80183480697dc5966d75b
Bartosz Dopke <bartoszdopke@gmail.com>
2019-11-13 13:31:55 +0100

    difficulty level added after Intro Test

---
+++ app/src/main/java/com/pracainzynierska/inzynierka/DailyChallengePopUpActivity.java
@@ -0,0 +1,14 @@
+package com.pracainzynierska.inzynierka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyChallengePopUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_challenge_pop_up);
+    }
+}
